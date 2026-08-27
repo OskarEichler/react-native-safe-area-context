0eb87c29c418e7062efe4e25ffd029a0abef269c
Oskar Eichler <62393985+OskarEichler@users.noreply.github.com>
2026-08-27 20:09:23 +0300

    style: format the changed web provider code

--- src/SafeAreaView.web.tsx
+++ src/SafeAreaView.web.tsx
@@ -53,7 +53,10 @@ export const SafeAreaView = React.forwardRef<
   }, [edges]);
 
   const appliedStyle = React.useMemo(() => {
-    const flatStyle = (StyleSheet.flatten(style) ?? {}) as Record<string, number>;
+    const flatStyle = (StyleSheet.flatten(style) ?? {}) as Record<
+      string,
+      number
+    >;
 
     if (mode === 'margin') {
       const {
